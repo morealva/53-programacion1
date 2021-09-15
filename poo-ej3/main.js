20443a31e6aae4c8b13ce5c98039768d44063d3d
morealva <emilcealvarez10@gmail.com>
2021-09-15 17:03:20 -0300

    catalogo anime

---
+++ poo-ej3/main.js
@@ -0,0 +1,9 @@
+import Anime from "./anime.js"
+
+const obtener = () => {
+    const instancia = new Anime ()
+    instancia.obtenerDatos()
+}
+
+const boton = document.getElementById("btn_obtener")
+boton.addEventListener("click",obtener)
